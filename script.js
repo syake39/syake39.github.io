56a7190a1de3dc8947760043060d6cb51593ab65
いわた <100102374+syake39@users.noreply.github.com>
2025-07-27 22:58:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// このスクリプトは、スクロールに応じて要素をフェードインさせるためのものです
+const targets = document.querySelectorAll('.fade-in-target');
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+targets.forEach(function(target) {
+    observer.observe(target);
+});
 
